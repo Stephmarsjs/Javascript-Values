c38640b03ff35cd9eddcb1be2995c577338f6e63
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-05-29 22:00:41 -0400

    added some javascript to get diffrent values

---
+++ practice.js
@@ -0,0 +1,33 @@
+console.log(`Half of 100 is ${100 / 2}`);
+
+// This outputs Half of 100 is 50
+
+console.log(3 > 2);
+
+// Boolean true or false
+
+console.log( true ? 1 : 2 ); // = 1
+console.log(false ? 1: 2); // = 2
+// ternary logic to pick one of two values based on a third value.
+
+console.log(null || "user");
+console.log("Stephanie" || "user");
+
+// The || operator will return the value that is to its left when that can be converted to true.
+
+let caught = (5*5);
+console.log(caught);
+
+let ten  = 10; 
+console.log(ten * ten);
+
+////////////////////
+let stephaniesDebt = 2000;
+stephaniesDebt = stephaniesDebt - 950;
+console.log(stephaniesDebt);
+
+var name = 'Valerie';
+const greeting = "Hiyah there ";
+console.log(greeting + name);
+
+if (1+1 == 2) console.log('Its true');
